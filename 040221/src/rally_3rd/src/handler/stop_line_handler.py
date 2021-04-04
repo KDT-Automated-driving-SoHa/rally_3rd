fc15b04ed11db5ec77eba792785448d709220fad
sinabeulo90 <smbyun0214@gmail.com>
2021-04-04 20:31:12 +0900

    실제 주행에서 사용한 코드

---
+++ 040221/src/rally_3rd/src/handler/stop_line_handler.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from handler import AbstractHandler
+from module.infos.motor_info import MotorInfo
+from module.stop_line.preprocessing import preprocessing_stopline
+from module.stop_line.stop_line import is_detect_crossline
+
+class StopLineHandler(AbstractHandler):
+
+    def __init__(self, speed=50):
+        self.speed = speed
+
+
+    def handle(self, handler_info):
+        frame = handler_info.image
+        prev_angle = handler_info.prev_angle
+        
+        preprcessed_frame = preprocessing_stopline(frame, thres_L=190)
+        ret, where = is_detect_crossline(preprcessed_frame, self.speed)
+
+        if ret:
+            motor_info_list = []
+            '''
+            # speed 50
+            cycles = [11, #<90
+                      14, #<190
+                      4, #<290
+                      3, #<390
+                      1, #<490
+                      1] #<600
+            '''
+            cycles = {90: 11,  #<90
+                      116: 17,
+                      135: 15,  # <190
+                      190: 17,  #<190
+                      200: 12,
+                      240: 10,  #<240
+                      310: 6,  #<290
+                      350: 5,  # <390
+                      390: 3,  #<390
+                      450: 1,  # <490
+                      490: 1,  #<490
+                      580: 2,
+                      600: 5} #<600
+
+            if where <=90:
+                motor_info_list.append(
+                    MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[90], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-3, iterations=2, delay_sec=0.05))
+            elif where < 116:
+                motor_info_list.append(
+                    MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[116], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-3, iterations=2, delay_sec=0.05))
+            elif where < 135:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[135], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-3, iterations=2, delay_sec=0.05))
+            elif where < 190:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[190], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-3, iterations=2, delay_sec=0.05))
+            elif where < 200:
+                motor_info_list.append(
+                    MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[200], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-3, iterations=2, delay_sec=0.05))
+            elif where < 240:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[240], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-3, iterations=2, delay_sec=0.05))
+            elif where < 310:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[310], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-4, iterations=4, delay_sec=0.05))
+            elif where < 350:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[350], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-4, iterations=4, delay_sec=0.05))
+            elif where < 390:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[390], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-4, iterations=4, delay_sec=0.05))
+            elif where < 450:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[450], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-4, iterations=9, delay_sec=0.05))
+            elif where < 490:
+                motor_info_list.append(MotorInfo(angle=0, speed=self.speed * 1 // 2, iterations=cycles[490], delay_sec=0.05))
+                motor_info_list.append(MotorInfo(angle=0, speed=-4, iterations=8, delay_sec=0.05))
+            elif where < 580:
+                motor_info_list.append(MotorInfo(angle=0, speed=-5, iterations=cycles[580], delay_sec=0.05))
+            else:
+                #a = 50
+                #b = 70
+                #cycle = int(round((600 - where + a) / b))
+                motor_info_list.append(MotorInfo(angle=0, speed=-5, iterations=cycles[600], delay_sec=0.05))
+                #motor_info_list.append(MotorInfo(angle=0, speed=-5, iterations=5, delay_sec=0.05))
+
+            motor_info_list.append(MotorInfo(angle=0, speed=0, iterations=58, delay_sec=0.1)) # stop for 6 sec
+            motor_info_list.append(MotorInfo(angle=0, speed=self.speed, iterations=10, delay_sec=0.1))
+            handler_info.laps_count += 1
+            print("laps:",handler_info.laps_count)
+            
+            return motor_info_list, handler_info
+        else:
+            return self._next_handler.handle(handler_info)
---
+++ 040221/src/rally_3rd/src/main.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import rospy
+import socket
+
+import cv2 as cv
+
+from collections import deque
+from datetime import datetime
+
+from module.ros_manager import RosManager
+from module.infos.handler_info import HandlerInfo
+from module.exposure.auto_exposure import AutoExposure
+
+from handler.stop_line_handler import StopLineHandler
+from handler.sliding_window_handler import SlidingWindowHandler
+from handler.hough_line_handler import HoughLineHandler
+from handler.ar_parking_handler import ARParkingHandler
+
+
+fps_dq = deque(maxlen=100)
+
+manager = RosManager()
+exposure = AutoExposure()
+
+stop_line_handler = StopLineHandler()           # 정지선 멈춤 처리
+sliding_window_handler = SlidingWindowHandler() # 슬라이딩 윈도우 처리
+hough_line_handler = HoughLineHandler()         # Hough Line 처리
+ar_parking_handler = ARParkingHandler()         # AR 주차
+
+# 책임 연쇄 초기화
+# ar_parking_handler.set_next(sliding_window_handler)
+
+ar_parking_handler.set_next(stop_line_handler)      # AR 주차 ---> 정지선 멈춤
+stop_line_handler.set_next(sliding_window_handler)  # 정지선 멈춤 ---> 슬라이딩 윈도우 주행
+# stop_line_handler.set_next(hough_line_handler)
+
+handler = ar_parking_handler
+prev_speed = 30
+prev_angle = 0
+laps_count = 0
+is_parking = False
+
+raw_input("준비~!")
+
+while not rospy.is_shutdown():
+    ret, frame = manager.get_image()
+
+    if ret is False:
+        continue
+
+    # Auto Exposure 적용
+    # value = exposure.get_exposure(frame)
+    # exposure.change_exposure(frame)
+
+    # # 프레임 수 검사
+    # fps_dq.append(datetime.now())
+    # fps = len(fps_dq) / ((fps_dq[-1]-fps_dq[0]).seconds + 1e-4)
+
+    # Handler 정보 저장
+    (lRet, lidar_info), (l2Ret, lidar_param) = manager.get_lidar()
+    uRet, ultrasonic_info = manager.get_ultrasonic()
+    (aRet1, ar_info1), (aRet2, ar_info2) = manager.get_ar()
+
+    handler_info = HandlerInfo()
+    handler_info.image              = frame
+    handler_info.lidar_info         = lidar_info if lRet else None
+    handler_info.lidar_param        = lidar_param if l2Ret else None
+    handler_info.ultrasonic_info    = ultrasonic_info if uRet else None
+    handler_info.ar_info1           = ar_info1 if aRet1 else None
+    handler_info.ar_info2           = ar_info2 if aRet2 else None
+    handler_info.laps_count         = laps_count
+    handler_info.prev_speed         = prev_speed
+    handler_info.prev_angle         = prev_angle
+    handler_info.is_parking         = is_parking
+
+    # 모터 정보 저장
+    motor_info, updated_handler_info = handler.handle(handler_info)
+    if type(motor_info) is list:
+        prev_speed = 50
+    else:
+        prev_speed = motor_info.speed
+        prev_angle = motor_info.angle
+        # motor_info.speed = 0
+        
+    laps_count = updated_handler_info.laps_count
+    is_parking = updated_handler_info.is_parking
+
+    # if type(motor_info) is not list:
+    #     motor_info.speed = 0
+    manager.publish_motor(motor_info)
+
+    if type(motor_info) != list:
+        prev_speed = motor_info.speed
+
+    # print "fps: {:>1.0f} | value: {:d} | {:s}".format(fps, value, motor_info)
+    # print "fps: {:>1.0f} | {:s}".format(fps, motor_info)
+
+    """
+    """
+    # Rendering
+    if socket.gethostname() == 'smbyeon-ubuntu':
+        cv.imshow("original", frame)
+        key = cv.waitKeyEx(1)
+        if key == ord('q'):
+            break
+    """
+    """
